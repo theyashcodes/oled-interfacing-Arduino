d01ed216ab8d87b8a65183f9c8094eadf1abf80d
theyashcodes <yash.gupta_cs.aiml24@gla.ac.in>
2026-02-11 10:15:22 +0530

    git commit -m "Add Doxygen documentation to src/main.cpp"

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3D
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    Wire.begin();
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+        Serial.println("OLED not found");
+        while (true);
+    }
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+
+    display.println("Arduino Uno R4");
+    display.println("OLED with I2C");
+    display.println("HELLO BOSS");
+
+    display.display();
 }
 
 void loop() {
-// write your code here
 }
